3ccdfc061ca673d6aaf6aae634712ec78eb34f67
ntphaoo <ntpthao1906@gmail.com>
2021-11-09 13:16:48 +0700

    search

--- client/src/pages/search/Search.jsx
+++ client/src/pages/search/Search.jsx
@@ -3,12 +3,14 @@ import Navbar from '../../components/navbar/Navbar'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './search.scss';
-
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 const Search = () => {
     const [showSearch, setShowSearch] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
     const [movies, setMovies] = useState([]);
-
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(!user);
     useEffect(() => {
         if (searchTerm.length > 0) {
             const searchMovie = async () => {
@@ -48,10 +50,42 @@ const Search = () => {
                 {!showSearch ? <h1>No Found</h1> :
                     movies.map((movie) => {
                         return (
+                           <>
+                            {!user ? (
+                                <Popup
+                                trigger={
+                                    <img src={movie.imgSm} alt={movie.title} key={movie._id} className="poster" />
+                                }
+                                modal
+                                nested
+                            >
+                            {close => (
+                                <div className="modal">
+                                    <button className="close" onClick={close}>
+                                        &times;
+                                    </button>
+                                    <div className="header">Notification</div>
+                                    <div className="content">
+                                        Please register or sign in to use this function
+                                    </div>
+                                    <div className="actions">
+                                        <Link to="/register"
+                                            className="button"
+                                        >
+                                            Register
+                                        </Link>
+                                    </div>
+                                </div>
+                            )}
+                        </Popup>
+                    ) : (
                             <Link to={{ pathname: '/info/'+movie._id, movie: movie }}>
                                 <img src={movie.imgSm} alt={movie.title} key={movie._id} className="poster" />
                             </Link>
+                    )}
+                    </>
                         )
+                        
                     })
                 }
             </div>
