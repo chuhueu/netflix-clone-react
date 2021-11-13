7c1b95296d40ec22f08282d3e00cfc01386b81fd
Hueu123 <chuhueu1@gmail.com>
2021-11-13 10:38:40 +0700

    commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,37 +1,40 @@
 import { useContext, useState } from "react";
 import { login } from "../../authContext/apiCalls";
 import { AuthContext } from "../../authContext/AuthContext";
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import "./login.scss";
-
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
-    
 import { isEmail, isEmpty } from 'validator';
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { dispatch } = useContext(AuthContext);
+  const { dispatch, error } = useContext(AuthContext);
   const handleLogin = (e) => {
     e.preventDefault();
     login({ email, password }, dispatch);
+    if (error) {
+      const element = <p>Wrong password or username</p>
+      ReactDOM.render(element, document.getElementById('error'));
+    }
   };
   const required = (value) => {
     if (isEmpty(value)) {
-        return <small className="form-text text-danger">This field is required</small>;
+      return <small className="form-text text-danger">This field is required</small>;
     }
   }
 
   const emaill = (value) => {
     if (!isEmail(value)) {
-        return <small className="form-text text-danger">Invalid email format</small>;
+      return <small className="form-text text-danger">Invalid email format</small>;
     }
   }
 
   const minLength = (value) => {
     if (value.trim().length < 6) {
-        return <small className="form-text text-danger">Password must be at least 6 characters long</small>;
+      return <small className="form-text text-danger">Password must be at least 6 characters long</small>;
     }
   }
 
@@ -50,28 +53,28 @@ export default function Login() {
         </div>
       </div>
       <div className="container">
-      <Form >
+        <Form >
           <h1>Sign In</h1>
           <Input
             type="email"
             placeholder="Email or phone number"
             onChange={(e) => setEmail(e.target.value)}
-            className="form-control" 
+            className="form-control"
             validations={[required, emaill]}
           />
           <Input
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
-            className="form-control" 
-            validations={[required, minLength]} 
+            className="form-control"
+            validations={[required, minLength]}
           />
           <button className="btn btn-info btn-block loginButton" onClick={handleLogin}>
             Sign In
           </button>
           {/* <CheckButton style={{ display: 'none' }} ref={c => { this.checkBtn = c }} /> */}
           <span>
-            New to Netflix? <Link to="/register" style={{textDecoration: "none"}}><b>Sign up now.</b></Link>
+            New to Netflix? <Link to="/register" style={{ textDecoration: "none" }}><b>Sign up now.</b></Link>
           </span>
           <small>
             This page is protected by Google reCAPTCHA to ensure you're not a
@@ -79,6 +82,7 @@ export default function Login() {
             <a href="https://www.google.com/recaptcha/about/" target="_blank" rel="noopener noreferrer" className="learn_more"><b>Learn more</b></a>
           </small>
         </Form>
+        <div id="error"></div>
       </div>
     </div>
   );
